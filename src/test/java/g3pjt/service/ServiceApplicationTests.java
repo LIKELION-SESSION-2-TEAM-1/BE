e4f90f3d8ec6bf5737cd0ebc8fca999d2d13962b
Trashbin4943 <anthonyhwan415@gmail.com>
2025-10-06 11:21:05 +0900

    initial

---
+++ src/test/java/g3pjt/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package g3pjt.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
