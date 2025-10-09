f71cfa1f7a696ff7004cfec677972689e4c55a48
Trashbin4943 <anthonyhwan415@gmail.com>
2025-10-09 14:43:20 +0900

    FEAT: 카카오맵 크롤링 기능 구현 초기

--- src/main/java/g3pjt/service/ServiceApplication.java
+++ src/main/java/g3pjt/service/ServiceApplication.java
@@ -2,12 +2,14 @@ package g3pjt.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceApplication.class, args);
+        SpringApplication.run(ServiceApplication.class, args);
 	}
 
 }
